832bf5a531e0d17bdb9906923f506c2fd3f61032
derilkillms <derilkillms@gmail.com>
2025-08-29 22:00:03 +0700

    object-request

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# request
+# Request
+
+## Instalation
+```
+composer require derilkillms/request
+```
+
+### Information
+
+**Request methods : ([wikipedia](https://en.wikipedia.org/wiki/HTTP#Request_methods))**
+HTTP defines methods (sometimes referred to as verbs, but nowhere in the specification does it mention verb) to indicate the desired action to be performed on the identified resource. What this resource represents, whether pre-existing data or data that is generated dynamically, depends on the implementation of the server. Often, the resource corresponds to a file or the output of an executable residing on the server. The HTTP/1.0 specification[49] defined the GET, HEAD, and POST methods as well as listing the PUT, DELETE, LINK and UNLINK methods under additional methods. However, the HTTP/1.1 specification[50] formally defined and added five new methods: PUT, DELETE, CONNECT, OPTIONS, and TRACE. Any client can use any method and the server can be configured to support any combination of methods. If a method is unknown to an intermediate, it will be treated as an unsafe and non-idempotent method. There is no limit to the number of methods that can be defined, which allows for future methods to be specified without breaking existing infrastructure. For example, WebDAV defined seven new methods and RFC 5789 specified the PATCH method..
+
+....
+
+**This Repository Based : PHP**
+```php
+require __DIR__ . '/vendor/autoload.php';
+
+use Derilkillms\Http\Request;
+
+session_start();
+$request = new Request();
+
+// Cek method
+echo $request->method(); // ex: POST
+
+// GET
+$id = $request->get('id');
+
+// POST
+$username = $request->post('username');
+
+// PUT
+$payload = $request->put();
+
+// PATCH
+$title = $request->patch('title');
+
+// DELETE
+$itemId = $request->delete('id');
+
+// SESSION
+$request->setSession('user_id', 42);
+echo $request->session('user_id');
+
+// Ambil semua data sesuai method aktif
+$data = $request->all();
+
+```
---
+++ src/Request.php
@@ -0,0 +1,120 @@
+<?php
+namespace Derilkillms\Http;
+
+class Request
+{
+    protected array $data = [];
+    protected array $session = [];
+    protected string $method;
+
+    public function __construct()
+    {
+        $this->method  = strtoupper($_SERVER['REQUEST_METHOD'] ?? 'GET');
+        $this->session = $_SESSION ?? [];
+
+        // Parse semua method
+        $this->data['GET']     = filter_input_array(INPUT_GET, FILTER_SANITIZE_SPECIAL_CHARS) ?? [];
+        $this->data['POST']    = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS) ?? [];
+        $this->data['PUT']     = $this->parseBody('PUT');
+        $this->data['PATCH']   = $this->parseBody('PATCH');
+        $this->data['DELETE']  = $this->parseBody('DELETE');
+        $this->data['OPTIONS'] = $this->parseBody('OPTIONS');
+        $this->data['HEAD']    = $this->parseBody('HEAD');
+    }
+
+    protected function parseBody(string $method): array
+    {
+        if ($this->method === $method) {
+            $raw = file_get_contents('php://input');
+            if ($this->isJson()) {
+                return json_decode($raw, true) ?? [];
+            }
+            parse_str($raw, $vars);
+            return filter_var_array($vars, FILTER_SANITIZE_SPECIAL_CHARS) ?? [];
+        }
+        return [];
+    }
+
+    protected function isJson(): bool
+    {
+        return isset($_SERVER['CONTENT_TYPE']) &&
+               stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false;
+    }
+
+    // Akses method spesifik
+    public function get(string $key = null, $default = null)
+    {
+        return $this->fetch('GET', $key, $default);
+    }
+
+    public function post(string $key = null, $default = null)
+    {
+        return $this->fetch('POST', $key, $default);
+    }
+
+    public function put(string $key = null, $default = null)
+    {
+        return $this->fetch('PUT', $key, $default);
+    }
+
+    public function patch(string $key = null, $default = null)
+    {
+        return $this->fetch('PATCH', $key, $default);
+    }
+
+    public function delete(string $key = null, $default = null)
+    {
+        return $this->fetch('DELETE', $key, $default);
+    }
+
+    public function options(string $key = null, $default = null)
+    {
+        return $this->fetch('OPTIONS', $key, $default);
+    }
+
+    public function head(string $key = null, $default = null)
+    {
+        return $this->fetch('HEAD', $key, $default);
+    }
+
+    // Session
+    public function session(string $key = null, $default = null)
+    {
+        return $this->fetchArray($this->session, $key, $default);
+    }
+
+    public function setSession(string $key, $value): void
+    {
+        $_SESSION[$key] = $value;
+        $this->session[$key] = $value;
+    }
+
+    // Generic fetch
+    protected function fetch(string $method, ?string $key, $default)
+    {
+        $array = $this->data[$method] ?? [];
+        return $this->fetchArray($array, $key, $default);
+    }
+
+    protected function fetchArray(array $array, ?string $key, $default)
+    {
+        if ($key === null) {
+            return $array;
+        }
+        return $array[$key] ?? $default;
+    }
+
+    // Info tambahan
+    public function method(): string
+    {
+        return $this->method;
+    }
+
+    public function all(string $method = null): array
+    {
+        if ($method === null) {
+            return $this->data[$this->method] ?? [];
+        }
+        return $this->data[strtoupper($method)] ?? [];
+    }
+}
